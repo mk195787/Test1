e8d7284f17c13611803a289b386a2716c2114a26
mk195787 <marcinkielbasinski@o2.pl>
2015-12-03 13:09:11 +0100

    Initial upload

---
+++ TesGitHub/src/mk195787/HelloWorld.java
@@ -0,0 +1,11 @@
+package mk195787;
+
+public class HelloWorld {
+	public static void main(String... args) {
+		System.out.println("Hello world!");
+	}
+	
+	public String getHelloWorld() {
+		return "Hello world!";
+	}
+}
---
+++ TesGitHub/src/mk195787/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package mk195787;
+
+// Unfinished
+public class HelloWorldTest {
+	
+	public void testHelloWorld() {
+		HelloWorld helloWorld = new HelloWorld();
+		String helloWorldString = helloWorld.getHelloWorld();
+	}
+}
